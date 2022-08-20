41782b3e5efacbbe2f3cb32cf492b51b285bd4bd
thezxdev <thezxdev@gmail.com>
2022-08-19 19:25:37 -0700

    git add .

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/system';
-import { NavBar } from '../components';
+import { NavBar, SideBar } from '../components';
 
 const drawerWidth = 240;
 
@@ -10,7 +10,7 @@ export const JournalLayout = ({ children }) => {
       {/* Navbar drawerWidth */}
       <NavBar drawerWidth={ drawerWidth } />
       {/* Sidebar drawerWidth */}
-
+      <SideBar drawerWidth={ drawerWidth } />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {/* Toolbar */}
 
