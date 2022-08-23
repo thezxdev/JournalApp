07dcd397ef98e3b08fea55f4ff84769d0cece386
thezxdev <thezxdev@gmail.com>
2022-08-22 22:52:41 -0700

    useForm hook en el RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,13 +1,27 @@
 // Ponerle un alias al Link del react-router-dom para usarlo con el Link de material
 import { Link as RouterLink } from 'react-router-dom';
-import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks/useForm';
+
+const formData = {
+  email: 'zxzx@gmail.com',
+  password: '123456',
+  displayName: 'zxzx'
+}
 
 export const RegisterPage = () => {
+
+  const { displayName, email, password, onInputChange, formState } = useForm( formData );
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log( formState );
+  }
+
   return (
     <AuthLayout title="Crear Cuenta">
-      <form>
+      <form onSubmit={ onSubmit }>
         <Grid container>
 
         <Grid item xs={12} sx={{ mt: 2 }}>
@@ -16,6 +30,9 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Nombre completo"
               fullWidth
+              name="displayName"
+              value={ displayName }
+              onChange={ onInputChange }
             />
           </Grid> {/* Nombre */}
 
@@ -25,6 +42,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="correo@google.com"
               fullWidth
+              name="email"
+              value={ email }
+              onChange={ onInputChange }
             />
           </Grid> {/* Correo */}
 
@@ -34,12 +54,15 @@ export const RegisterPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name="password"
+              value={ password }
+              onChange={ onInputChange }
             />
           </Grid> {/* Contraseña */}
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Crear cuenta
               </Button>
             </Grid> {/* Login */}
