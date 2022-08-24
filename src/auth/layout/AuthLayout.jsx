cce3fda6a0e52c538e3adce4aa921dcde126f383
thezxdev <thezxdev@gmail.com>
2022-08-23 22:24:11 -0700

    CheckingAuth component (mostrar spinner mientras se comprueba si se tiene sesión iniciada o no)

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -14,7 +14,7 @@ export const AuthLayout = ({ children, title = '' }) => {
     <Grid item
       className="box-shadow"
       xs={ 3 }
-      sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2, width: { md: 450 } }}
+      sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2, width: { sm: 450 } }}
     >
       <Typography variant='h5' sx={{ mb: 1 }}>{ title }</Typography>
 
