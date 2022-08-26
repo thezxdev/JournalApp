dd68ee185ed010be35d628e03804b401ba1b74e0
thezxdev <thezxdev@gmail.com>
2022-08-26 15:34:16 -0700

    Creacion del journalSlice y sus reducers

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+  name: 'journal',
+  initialState: {
+    isSaving: true,
+    messageSaved: '',
+    notes: [],
+    active: null
+    // active: {
+    //   id: 'ABC123',
+    //   title: '',
+    //   body: '',
+    //   date: 1234567,
+    //   imagesUrls: [], // https://foto1.jpg, https://foto2.jpg, ...
+    // }
+  },
+  reducers: {
+    addNewEmptyNote: ( state, action ) => {
+
+    },
+    setActiveNote: ( state, action ) => {
+
+    },
+    setNotes: ( state, action ) => {
+      
+    },
+    setSaving: ( state ) => {
+
+    },
+    updateNote: ( state, action ) => {
+
+    },
+    deleteNoteById: ( state, action ) => {
+
+    },
+  }
+});
+// Action creators are generated for each case reducer function
+export const {
+  addNewEmptyNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+  deleteNoteById
+} = journalSlice.actions;
