d5e00a0b1cc50802d0a5ef55567a62d303a1a079
thezxdev <thezxdev@gmail.com>
2022-08-28 01:20:50 -0700

    Creacion del SideBarItem component y mostrar las notas en el menu lateral

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,10 +1,11 @@
-import { TurnedInNot } from '@mui/icons-material';
-import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { SideBarItem } from './';
 
 export const SideBar = ({ drawerWidth = 240 }) => {
   
   const { displayName } = useSelector( state => state.auth );
+  const { notes } = useSelector( state => state.journal );
   
   return (
     <Box
@@ -25,18 +26,11 @@ export const SideBar = ({ drawerWidth = 240 }) => {
 
         <List>
           {
-            ['Enero', 'Febrero', 'Marzo', 'Abril'].map( text => (
-              <ListItem key={ text } disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <TurnedInNot />
-                  </ListItemIcon>
-                  <Grid container>
-                    <ListItemText primary={ text } />
-                    <ListItemText secondary={ 'Ipsum laborum ipsum labore exercitation cillum excepteur do amet.' } />
-                  </Grid>
-                </ListItemButton>
-              </ListItem>
+            notes.map( note => (
+              <SideBarItem
+                key={ note.id }
+                { ...note } // Esparcir los valores de la nota para obtenerlo en las props
+              />
             ))
           }
         </List>
