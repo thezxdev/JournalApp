df1863a5eb72aff88dfa49eb4d7d7ae4d25fc127
thezxdev <thezxdev@gmail.com>
2022-08-29 11:50:20 -0700

    SideBarItem Activar nota

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,9 +1,13 @@
-import { TurnedInNot } from '@mui/icons-material'
-import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-import { useMemo } from 'react'
+import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { TurnedInNot } from '@mui/icons-material';
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { setActiveNote } from '../../store/journal';
 
-export const SideBarItem = ({ title, body, id }) => {
+export const SideBarItem = ({ title, body, id, date, imageUrls = [] }) => {
 
+  const dispatch = useDispatch();
+  
   const newTitle = useMemo( () => {
 
     return title.length > 17
@@ -12,9 +16,16 @@ export const SideBarItem = ({ title, body, id }) => {
 
   }, [ title ] );
 
+  // Activar nota
+  const onClickNote = () => {
+    dispatch( setActiveNote( { title, body, id, date, imageUrls } ) )
+  }
+
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton
+        onClick={ () => onClickNote({ title, body, id }) }
+      >
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
